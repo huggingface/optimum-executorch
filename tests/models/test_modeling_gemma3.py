7f1a885c5cfcda77d68c5dcfc6525f74e39bcb77
Kimish Patel <kimishpatel@fb.com>
2025-06-06 20:12:42 -0700

    Add support for sliding window via ring kv cache
    Summary:
    This diffs adds
    - Ring kv cache for local attention where ring buffer updates cache
      with new entries in a ring fashion rather than sliding cache
    - Correspondingly we have to update sdpa to accept mask that correspond
      to the state of ring buffer because slot in ring buffer may correspond
      to different token positions. Thus we cannot just use causal mask.
    
    Now custom_kv_cache option means we support both global attention and
    local/global like gemma3
    
    Test Plan:
    Test added
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- tests/models/test_modeling_gemma3.py
+++ tests/models/test_modeling_gemma3.py
@@ -23,11 +23,13 @@ import unittest
 
 import pytest
 import torchao
+import transformers
 from executorch.extension.pybindings.portable_lib import ExecuTorchModule
 from packaging.version import parse
 from transformers import AutoTokenizer
 from transformers.testing_utils import slow
 
+from executorch import version
 from optimum.executorch import ExecuTorchModelForCausalLM
 from optimum.utils.import_utils import is_transformers_version
 
@@ -195,6 +197,44 @@ class ExecuTorchModelIntegrationTest(unittest.TestCase):
             model_id,
             recipe="xnnpack",
             attn_implementation="custom_sdpa",
+            **kwargs,
+        )
+        self.assertIsInstance(model, ExecuTorchModelForCausalLM)
+        self.assertIsInstance(model.model, ExecuTorchModule)
+
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        generated_text = model.text_generation(
+            tokenizer=tokenizer,
+            prompt=prompt,
+            max_seq_len=64,
+        )
+        logging.info(f"\nGenerated text:\n\t{generated_text}")
+        generated_tokens = tokenizer(generated_text, return_tensors="pt").input_ids
+
+        # Free memory before loading eager for quality check
+        del model
+        del tokenizer
+        gc.collect()
+
+        self.assertTrue(check_causal_lm_output_quality(model_id, generated_tokens))
+
+    @pytest.mark.skipif(
+        parse(transformers.__version__) < parse("4.52.0") or parse(torchao.__version__) < parse("0.11.0") or parsee(version.__version__) <= parse("0.6.0"),
+        reason="Only available on transformers >= 4.52.0 and torchao >= 0.11.0 executorch > 0.6.0",
+    )
+    def test_gemma3_text_generation_with_custom_sdpa_kv_cache_8da4w_8we(self):
+        # TODO: Until https://github.com/huggingface/optimum/issues/2127 is fixed, have to use non-gated model on CI
+        # model_id = "google/gemma-3-1b-it"
+        model_id = "unsloth/gemma-3-1b-it"
+        prompt = "Write a poem about a machine learning."
+
+        # ExecuTorch model + custom sdpa + 8da4w linear quantization + int8 embedding quantization
+        kwargs = {"qlinear": True, "qembedding": True}
+        model = ExecuTorchModelForCausalLM.from_pretrained(
+            model_id,
+            recipe="xnnpack",
+            attn_implementation="custom_sdpa",
+            use_custom_kv_cache=True,
             **kwargs,
         )
         self.assertIsInstance(model, ExecuTorchModelForCausalLM)
