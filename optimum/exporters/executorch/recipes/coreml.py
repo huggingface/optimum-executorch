ed5e80c34f944a8a4a00d7908d6e1ec3ff74c7b6
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-06-13 16:51:02 -0700

    up

--- optimum/exporters/executorch/recipes/coreml.py
+++ optimum/exporters/executorch/recipes/coreml.py
@@ -15,13 +15,10 @@
 import logging
 from typing import Dict, Union
 
-from packaging.version import parse
 from tabulate import tabulate
 from torch.export import ExportedProgram
 import coremltools as ct
-import torch
 
-from executorch import version as executorch_version
 from executorch.backends.apple.coreml.partition import CoreMLPartitioner
 from executorch.backends.apple.coreml.compiler import CoreMLBackend
 
@@ -32,39 +29,12 @@ from executorch.exir import (
     ExecutorchProgram,
     to_edge_transform_and_lower,
 )
-from optimum.executorch.passes.remove_padding_idx_embedding_pass import RemovePaddingIdxEmbeddingPass
-from executorch.backends.apple.coreml.quantizer import CoreMLQuantizer
-from torchao.quantization.pt2e.quantize_pt2e import convert_pt2e, prepare_pt2e
 from ..integrations import (
     CausalLMExportableModule,
     MaskedLMExportableModule,
     Seq2SeqLMExportableModule,
 )
 from ..recipe_registry import register_recipe
-
-def get_quantization_config():
-    quantization_config = ct.optimize.torch.quantization.LinearQuantizerConfig.from_dict(
-        {
-            "global_config": {
-                "quantization_scheme": ct.optimize.torch.quantization.QuantizationScheme.symmetric,
-                "activation_dtype": torch.quint8,
-                "weight_dtype": torch.qint8,
-                "weight_per_channel": True,
-            }
-        }
-    )
-    return quantization_config
-
-def quantize_program(ep):
-    quantizer = CoreMLQuantizer(get_quantization_config())
-    gm = ep.module()
-    
-    args, kwargs = ep.example_inputs
-    prepared_model = prepare_pt2e(gm, quantizer) 
-    prepared_model(*args, **kwargs)
-    converted_model = convert_pt2e(prepared_model)
-    return torch.export.export(converted_model, args, kwargs)
-
 
 @register_recipe("coreml")
 def export_to_executorch_with_coreml(
@@ -94,43 +64,30 @@ def export_to_executorch_with_coreml(
         **kwargs,
     ) -> Dict[str, ExecutorchProgram]:
 
-        minimum_deployment_target = kwargs.get("minimum_ios_deployment_target", "15")
-        minimum_deployment_target = {
-            "15": ct.target.iOS15,
-            "16": ct.target.iOS16,
-            "17": ct.target.iOS17,
-            "18": ct.target.iOS18,
-        }[minimum_deployment_target]
-
-        compute_precision = kwargs.get("compute_precision", "fp16")
-        compute_precision = {
-            "fp16": ct.precision.FLOAT16,
-            "fp32": ct.precision.FLOAT32,
-        }[compute_precision]
-
+        compute_unit = kwargs.get("compute_unit", ct.ComputeUnit.ALL)
+        minimum_deployment_target = kwargs.get("minimum_deployment_target", ct.target.iOS15)
+        compute_precision = kwargs.get("compute_precision", ct.precision.FLOAT16)
         model_type = kwargs.get("model_type", "model")
         model_type = {
             "model": CoreMLBackend.MODEL_TYPE.MODEL,
             "modelc": CoreMLBackend.MODEL_TYPE.COMPILED_MODEL,
         }[model_type]
         take_over_mutable_buffer = kwargs.get("take_over_mutable_buffer", True)
-        quantize = kwargs.get("quantize", False)
 
         et_progs = {}
         backend_config_dict = {}
         for pte_name, exported_program in exported_programs.items():
             logging.debug(f"\nExported program for {pte_name}.pte: {exported_program}")
-            if quantize:
-                exported_program = quantize_program(exported_program)
             et_progs[pte_name] = to_edge_transform_and_lower(
                 exported_program,
                 partitioner=[CoreMLPartitioner(
                     compile_specs=CoreMLBackend.generate_compile_specs(
+                        compute_unit=compute_unit,
                         minimum_deployment_target=minimum_deployment_target,
                         compute_precision=compute_precision,
                         model_type=model_type,
                     ),
-                    take_over_mutable_buffer=take_over_mutable_buffer, # Fails when set to true
+                    take_over_mutable_buffer=take_over_mutable_buffer,
                 )],
                 compile_config=EdgeCompileConfig(
                     _check_ir_validity=False,
