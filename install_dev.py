c147b785fcb3b5cf892d0a0211b81e6c9216dbfb
Scott Roy <161522778+metascroy@users.noreply.github.com>
2025-07-10 20:06:22 -0700

    up

--- install_dev.py
+++ install_dev.py
@@ -8,7 +8,7 @@ def install_torch_nightly_deps():
     EXECUTORCH_NIGHTLY_VERSION = "dev20250629"
     TORCHAO_NIGHTLY_VERSION = "dev20250620"
     # Torch nightly is aligned with pinned nightly in https://github.com/pytorch/executorch/blob/main/install_requirements.py#L74
-    TORCH_NIGHTLY_VERSION = "dev20250601"
+    TORCH_NIGHTLY_VERSION = "dev20250625"
     subprocess.check_call(
         [
             sys.executable,
