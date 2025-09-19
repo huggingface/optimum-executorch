cd80db2c804ba4f2806b4e299f0882e7ab0b4579
Jack <32371937+jackzhxng@users.noreply.github.com>
2025-09-19 11:17:38 -0400

    Update install_dev.py
    Try 0905

--- install_dev.py
+++ install_dev.py
@@ -5,10 +5,10 @@ import sys
 
 def install_torch_nightly_deps():
     """Install torch related dependencies from pinned nightly"""
-    EXECUTORCH_NIGHTLY_VERSION = "dev20250906"
-    TORCHAO_NIGHTLY_VERSION = "dev20250906"
+    EXECUTORCH_NIGHTLY_VERSION = "dev20250905"
+    TORCHAO_NIGHTLY_VERSION = "dev20250905"
     # Torch nightly is aligned with pinned nightly in https://github.com/pytorch/executorch/blob/main/install_requirements.py#L74
-    TORCH_NIGHTLY_VERSION = "dev20250906"
+    TORCH_NIGHTLY_VERSION = "dev20250905"
     subprocess.check_call(
         [
             sys.executable,
